89a17e0007e17a586f979608971d8acffe84a670
ompakap <oom34299@gmail.com>
2016-09-14 23:18:41 +0700

    add logo

--- public/js/89.js
+++ public/js/89.js
@@ -1,4 +1,9 @@
-var BASE_URL = 'http://localhost:83/';
-var BASE_LANG = 'http://localhost:83/th/';
+//var BASE_URL = 'http://localhost:83/';
+//var BASE_LANG = 'http://localhost:83/th/';
+
+var BASE_URL = 'http://jellysthailand.com/';
+var BASE_LANG = 'http://jellysthailand.com/th/';
+
+
 var SLANG = 'th';
 var LANG = 'tha';
