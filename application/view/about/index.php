8efac8c60595764870259cef4c2e84b707fb65ef
ompakap <oom34299@gmail.com>
2017-02-14 00:25:19 +0700

    fix aboutus

--- application/view/about/index.php
+++ application/view/about/index.php
@@ -15,6 +15,17 @@
 
 	<div class="wrapContent row">
 		<div class="col-sm-5 nopad">
+			
+		</div>
+		<div class="col-sm-6 col-sm-offset-1 nopad">
+			<div class="wrap_right_content">
+				<h1>บริษัท เอ็นทีบีพลัส (ไทยแลนด์) จำกัด</h1>
+				<p>เป็นหนึ่งในผู้ผลิต จัดจำหน่าย และส่งออกผลิตภัณฑ์บำรุงผิว (SkinCare)ภายใต้แบรนด์ “JELLYS” ซึ่งเป็นที่รู้จักกันอย่างกว้างขวางในปัจจุบันและได้รับการยอมรับทั่วโลก อาทิ อินโดนีเซีย มาเลเซีย ฮ่องกง พม่า ลาว กัมพูชาฟิลิปปินส์ สิงคโปร์ ซาอุดิอาระเบีย และประเทศตะวันออกกลางความภาคภูมิใจของ บริษัท เอ็นทีบีพลัส (ไทยแลนด์) จำกัดผู้บริหารการจัดจำหน่ายผลิตภัณฑ์ ในภูมิภาค ประเทศไทย
+				</p>
+				<p class="last">โดยดำเนินธุรกิจภายใต้คอนเซปต์ &quot;SKIN CARE IS HEALTHY&quot; ที่มาจากแนวความเชื่อที่ว่า การดูแลผิวที่มีสุขภาพดีในแบบที่เป็นคุณ ……เพราะเราเชื่อว่าผิวพรรณของผู้หญิงที่แท้จริงนั้น มาจากการดูแลผิวพรรณให้มีสุขภาพที่ดีเผยความกระจ่างใสในตัวคุณ
+				</p>
+			</div>
+
 			<div class="wrap_left_content">
 				<div class="wrap_left_position_content hidden-xs">
 					<p class="txt_quote">“</p>
@@ -31,17 +42,7 @@
 					<p class="txt_quote">”</p>
 				</div>
 			</div>
-		</div>
-		<div class="col-sm-6 col-sm-offset-1 nopad">
-			<div class="wrap_right_content">
-				<h1>บริษัท เอ็นทีบีพลัส (ไทยแลนด์) จำกัด</h1>
-				<p>เป็นหนึ่งในผู้ผลิต จัดจำหน่าย และส่งออกผลิตภัณฑ์บำรุงผิว (SkinCare)ภายใต้แบรนด์ “JELLYS” ซึ่งเป็นที่รู้จักกันอย่างกว้างขวางในปัจจุบันและได้รับการยอมรับทั่วโลก อาทิ อินโดนีเซีย มาเลเซีย ฮ่องกง พม่า ลาว กัมพูชาฟิลิปปินส์ สิงคโปร์ ซาอุดิอาระเบีย และประเทศตะวันออกกลางความภาคภูมิใจของ บริษัท เอ็นทีบีพลัส (ไทยแลนด์) จำกัดผู้บริหารการจัดจำหน่ายผลิตภัณฑ์ ในภูมิภาค ประเทศไทย
-				</p>
-				<p class="last">โดยดำเนินธุรกิจภายใต้คอนเซปต์ &quot;SKIN CARE IS HEALTHY&quot; ที่มาจากแนวความเชื่อที่ว่า การดูแลผิวที่มีสุขภาพดีในแบบที่เป็นคุณ ……เพราะเราเชื่อว่าผิวพรรณของผู้หญิงที่แท้จริงนั้น มาจากการดูแลผิวพรรณให้มีสุขภาพที่ดีเผยความกระจ่างใสในตัวคุณ
-				</p>
-				<br>
-				<br>
-			</div>
+
 		</div>
 	</div>
 
@@ -52,23 +53,28 @@
     background-repeat: no-repeat;
 	background-size: cover;background-position: center center;  ">
 	<div class="wrapContent">
-		<div class="detail_content">
+		<div class="detail_content col-xs-12 col-sm-9 col-md-9">
 			บริษัทฯ มุ่งมั่นที่จะพัฒนาผลิตภัณฑ์อย่างสร้างสรรค์ให้มีความแปลกใหม่และมีคุณภาพสูงเพื่อตอบสนองความต้องการของลูกค้าที่หลากหลาย โดยบริษัทฯใส่ใจในทุกขั้นตอนการผลิต เพื่อให้ได้มาตรฐานและประโยชน์สูงสุดต่อผู้บริโภคทั้งในประเทศและต่างประเทศโดยคำนึงถึงประสิทธิภาพและคุณภาพเพื่อลูกค้าเป็นสำคัญ ภายใต้ปรัชญาองค์กรที่ว่า <span class="content_bold">“สร้างสรรค์ มุ่งมั่น ก้าวไกล”</span> ด้วย 4 พันธกิจหลัก คือ 
 		</div>
+		<div class="clearfix"></div>
 		<ul class="list_content">
 			<li><span>1. </span>พัฒนา คิดค้น สร้างสรรค์ และยกระดับคุณภาพผลิตภัณฑ์</li>
 			<li><span>2. </span>ขยายช่องทางการขายและการตลาดเพื่อให้ลูกค้าสามารถเข้าถึงและเป็น Top-of-mind ของลูกค้า</li>
 			<li><span>3. </span>ปรับโครงสร้างองค์กรให้มีความคล่องตัวพร้อมต่อการเปลี่ยนแปลง และโอกาสทางธุรกิจ</li>
 			<li><span>4. </span>รับผิดชอบต่อสังคมและสิ่งแวดล้อมโดยคำนึงถึงประโยชน์ส่วนรวมเป็นที่ตั้ง</li>
 		</ul>
-		<div class="detail_content detail_content_bottom">เพื่อมุ่งสู่เป้าหมายในการเป็นผู้นำการผลิตและจัดจำหน่ายผลิตภัณฑ์บำรุงผิวและผลิตภัณฑ์อาหารเสริมชั้นนำของโลก (World Leader of Skin Care) ต่อไป
+		<div class="detail_content detail_content_bottom  col-xs-12 col-sm-9 col-md-9">เพื่อมุ่งสู่เป้าหมายในการเป็นผู้นำการผลิตและจัดจำหน่ายผลิตภัณฑ์บำรุงผิวและผลิตภัณฑ์อาหารเสริมชั้นนำของโลก (World Leader of Skin Care) ต่อไป
 		</div>
+		<div class="clearfix"></div>
 	</div>
+	
+	<div class="clearfix"></div>
 
 	<div style=" position: relative; background: #f8f8f8 url(/public/images/aboutus/aboutus03.png);
     background-repeat: no-repeat;
-	background-size: cover; background-position: center center; ">
-		<div class="wrapContent">
+	    background-size: cover;
+    background-position: 0px -131px;">
+		<div class="col-md-7 col-md-offset-5">
 			<div class="detail_content">
 				<h3>Jellys PRODUCTS</h3>
 				<p>เจลลี่ มุ่งมั่นนำเสนอผลิตภัณฑ์สกินแคร์ดูแลผิว ที่คัดสรรวัตถุดิจากธรรมชาติเพื่อให้ได้มาซึ่งผลิตภัณฑ์บำรุงผิวที่มีคุณภาพมาตรฐาน</p>
@@ -79,6 +85,9 @@
 			<div class="detail_content detail_content_bottom">ดังนั้นผู้บริโภคจึงมั่นใจได้ว่า สินค้าจากJellysจะมีความปลอดภัย และมีคุณภาพสูงอย่างแท้จริง
 			</div>
 		</div>
+
+		<div class="clearfix"></div>
+
 	</div>
 
 </div>
